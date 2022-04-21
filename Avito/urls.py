73c99af216e4f4d37c4b1d33b0389031bc01ac78
saybe <Sayberol>
2022-04-21 15:56:08 +0500

    refactor

--- Avito/urls.py
+++ Avito/urls.py
@@ -1,4 +1,4 @@
-"""ads URL Configuration
+"""Avito URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-import views
+from new_ads import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
